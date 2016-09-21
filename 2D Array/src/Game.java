9bd5e59bdeb200bfdf6b35375ca6830ec5c994df
Zhiyuan Huang <Huangzhiyuan999@gmail.com>
2016-09-21 09:45:23 -0600

    Fixed fill star method

--- 2D Array/src/Game.java
+++ 2D Array/src/Game.java
@@ -7,7 +7,7 @@ public class Game
 	{
 		static String name;
 		static int waterLevel = 10;
-		static int playerRow = 19;
+		static int playerRow = 18;
 		static int playerCol = 10;
 		static int numOfFood = 10;
 		static int health = 100;
@@ -430,105 +430,27 @@ public class Game
 								System.out.println(" ");
 								for(int col = 0; col<arrayGame[0].length;col++)
 									{
-										int letter=(int)(Math.random()*75)+1;
-										switch (letter)
-										{
-											case 1:
-											case 2:
-											case 3:
-											case 4:
-											case 5:
-											case 6:
-											case 7:
-											case 8:
-											case 9:
-											case 10:
-											case 11:
-											case 12:
-											case 13:
-											case 14:
-											case 15:
-											case 16:
-											case 17:
-											case 18:
-											case 19:
-											case 20:
-											case 21:
-											case 22:
-											case 23:
-											case 24:
-											case 25:
-											case 26:
-											case 27:
-											case 28:
-											case 29:
-											case 30:
-											case 31:
-											case 32:
-											case 33:
-											case 34:
-											case 35:
-											case 36:
-											case 37:
-											case 38:
-											case 39:
-											case 40:
-											case 41:
-											case 42:
-											case 43:
-											case 44:
-											case 45:
-											case 46:
-											case 47:
-											case 48:
-											case 49:
-											case 50:
-											case 51:
-											case 52:
-											case 53:
-											case 54:
-											case 55:
-											case 56:
-											case 57:
-											case 58:
-											case 59:
-											case 60:
-											case 61:
-											case 62:
-											case 63:
-											case 64:
-											case 65:
-											case 66:
-											case 67:
-											case 68:
-											case 69:
-											case 70:
-											case 71:
-													{
-														arrayGame[row][col]=" ~ ";
-														break;
-													}
-											case 72:
-													{
-														arrayGame[row][col]=" h ";
-														break;
-													}
-											case 73:
-													{
-														arrayGame[row][col]=" m ";
-														break;
-													}
-											case 74:
-													{
-														arrayGame[row][col]=" c ";
-														break;
-													}
-											case 75:
-													{
-														arrayGame[row][col]=" v ";
-														break;
-													}
-										}
+										int letter = (int)(Math.random()*50)+1;
+										if (letter == 50)
+											{
+												arrayGame[row][col]=" v ";
+											}
+										else if (letter == 49)
+											{
+												arrayGame[row][col]=" c ";
+											}
+										else if (letter == 48)
+											{
+												arrayGame[row][col]=" m ";
+											}
+										else if (letter == 47)
+											{
+												arrayGame[row][col]=" h ";
+											}
+										else
+											{
+												arrayGame[row][col]=" ~ ";
+											}
 									}
 								arrayGame[playerRow][playerCol]=" @ ";
 							}
